b405c3cdd49b2bf03b89698838a7349139ff8807
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-02-25 12:39:48 +0100

    feat: logs and perfomance optimization

--- www/index.js
+++ www/index.js
@@ -6,6 +6,54 @@ const GRID_COLOR = '#3E3E3E';
 const DEAD_COLOR = '#121212';
 const ALIVE_COLOR = '#FFFFFF';
 
+// Frame per Second Timer for performance statistics
+const fps = new (class {
+  constructor() {
+    this.fps = document.getElementById('fps');
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // convert the detail time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    // save only the latest 100 timings
+    this.frames.push(fps);
+
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+
+    let mean = sum / this.frames.length;
+
+    // render the statistics.
+    this.fps.textContent = `
+Frames per Second: 
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+})();
+
 // Construct the universe room form all of our cells and a 1px border
 const universe = Universe.new();
 const width = universe.width();
@@ -46,11 +94,37 @@ const drawCells = () => {
 
   ctx.beginPath();
 
+  // Alive cells
+  ctx.fillStyle = ALIVE_COLOR;
+
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
 
-      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+      if (cells[idx] !== Cell.Alive) {
+        continue;
+      }
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE,
+      );
+    }
+  }
+
+  // Dead cells
+  ctx.fillStyle = DEAD_COLOR;
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      if (cells[idx] !== Cell.Dead) {
+        continue;
+      }
+
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
         row * (CELL_SIZE + 1) + 1,
@@ -66,6 +140,8 @@ const drawCells = () => {
 let animationId = null;
 
 const renderLoop = () => {
+  fps.render();
+
   universe.tick();
 
   drawGrid();
